f666248de16cb66325c7c22258bfbfd25351802a
the-general-lee <ahmedosama001@aucegypt.edu>
2023-03-25 12:40:12 +0200

    feat(front): remove debugger instances

--- portfolio/src/views/bar/index.jsx
+++ portfolio/src/views/bar/index.jsx
@@ -3,7 +3,6 @@ import Header from "../../components/Header";
 import BarChart from "../../components/BarChart";
 
 const Bar = () => {
-  debugger;
   return (
     <Box m="20px">
       <Header title="Bar Chart" subtitle="Simple Bar Chart" />
