b544182f44729073517a8db6af462fbe696b25fa
the-general-lee <ahmedosama001@aucegypt.edu>
2023-03-28 06:01:54 +0300

    feat(front): do

--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -3,7 +3,10 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-
+/* for deployment on pages:
+https://blog.logrocket.com/deploying-react-apps-github-pages/
+https://dev.to/kathryngrayson/deploying-your-cra-react-app-on-github-pages-2614
+ */
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <>
